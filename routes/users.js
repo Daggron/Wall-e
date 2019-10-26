7619420432fd082b6922b5b9c8a848ac15418b07
Daggron <returnofking04@gmail.com>
2019-10-26 23:04:35 +0530

    Now you can search images

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,8 @@ router.get('/',async (req,res)=>{
     let data = await unsplash.photos.listPhotos(2,30,'wallpaper');
     let response = await data.json();
 
-    console.log(response);
-    let images = {full,raw,small} = response.map(response =>{
+    // console.log(response);
+     req.session.images = {full,raw,small} = response.map(response =>{
         return {
             full:response.urls.full,
             raw:response.urls.raw,
@@ -22,11 +22,35 @@ router.get('/',async (req,res)=>{
         }
     });
 
+
     res.render("index",{
-        data:images
-    })
+        data:req.session.images
+    });
     
 });
 
+router.post('/',async (req,res)=>{
+    console.log(req.body.keyword);
+    let data = await unsplash.search.photos(req.body.keyword,1);
+    let response = await data.json();
+
+    // console.log(response.results);
+
+    // res.send(response.results[0].urls);
+
+     req.session.images = {full,raw,small} = response.results.map(response =>{
+        return {
+            full:response.urls.full,
+            raw:response.urls.raw,
+            small:response.urls.small,
+        }
+
+    });
+
+    res.render('index',{
+        data:req.session.images
+    })
+    
+})
 
 module.exports = router;
