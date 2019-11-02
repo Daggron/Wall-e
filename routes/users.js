d8b3026b94779a557667fcb2e0354f8fbf295052
Daggron <returnofking04@gmail.com>
2019-11-02 11:48:27 +0530

    Finalizing the project

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,47 @@
 const router = require('express').Router();
 const Unsplash  = require('unsplash-js').default;
 const fetch = require('node-fetch');
+const Axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 global.fetch = fetch;
 require('dotenv/config');
+
 
  const unsplash = new Unsplash({
     accessKey:process.env.KEY
 })
+
+
+
 
 router.get('/',async (req,res)=>{
 
     // eval(require('locus'));
 
     if(req.query.search === undefined){
-        console.log("Helllllllo")
+        // console.log("Helllllllo")
+
     let data = await unsplash.photos.listPhotos(2,30,'wallpaper');
     let response = await data.json();
 
-     req.session.images = {full,raw,small} = response.map(response =>{
+    // console.log(response);
+
+     req.session.images = {full,raw,small} = await response.map(response =>{
         return {
             full:response.urls.full,
             raw:response.urls.raw,
             small:response.urls.small,
+            download : response.links.download,
         }
     });
 
-
     res.render("index",{
-        data:req.session.images
+        data:req.session.images,
     });
     }else{
 
-        console.log(req.query.search)
+        // console.log(req.query.search)
 
         let data = await unsplash.search.photos(req.query.search,1);
         let response = await data.json();
@@ -41,11 +51,14 @@ router.get('/',async (req,res)=>{
             return {
                 full:response.urls.full,
                 raw:response.urls.raw,
-                small:response.urls.small,                
+                small:response.urls.small,    
+                download : response.links.download,            
             }
     
         });
-    
+        
+        // downloadImage(req.session.images[0].download);
+      
         res.render('index',{
             data:req.session.images
         })
